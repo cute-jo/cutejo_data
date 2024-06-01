b2533a578f10ed05be05c03217caf20d0660dced
cutejo <163100211+cute-jo@users.noreply.github.com>
2024-06-02 07:58:00 +0900

    Add files via upload
    Signed-off-by: cutejo <163100211+cute-jo@users.noreply.github.com>

---
+++ su-android/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AndriodProject_cute-jo"
+include(":app")
+ 
